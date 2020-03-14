033fb379b7efb180c6ed0766987e6169ab7ad3d6
alessandrobessi <alessandro.bessi@mail.com>
2020-03-14 18:39:54 +0100

    :chart_with_upwards_trend: add charts and style, update readme

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # covid-italy-monitoring
 A script to visualize some statistics about the CoVid-19 epidemics in Italy
+
+### Usage
+```shell script
+pip install -r requirements.txt
+python3 main.py
+```
+
+## Example
+The script produces the following report:
+```
+--------------------------------------------------
+2020-03-14 Report
+--------------------------------------------------
+Total number of infected individuals is 21157
+Total number of recovered individuals is 1966
+Total number of dead individuals is 1441
+Total number of tested individuals is 109170
+--------------------------------------------------
+Current number of infected individuals is 17750
+--- hospitalized individuals is 8372
+--- hospitalized individuals in ICU is 1518
+--- home isolated individuals is 7860
+--------------------------------------------------
+Number of new infected is 3497
+Growth rate is 0.20 (5 days smoothing is 0.18)
+--------------------------------------------------
+```
+
+and the following image:
+![example image][example]
+
+[example]: example.png "Logo
+ Title Text 2"
--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 import matplotlib.pyplot as plt
 import numpy as np
 import requests
@@ -17,37 +19,67 @@ if __name__ == '__main__':
 
     data = raw_data.json()
 
-    dates = [d['data'][:10] for d in data]
+    dates = [datetime.strptime(d['data'][:10], '%Y-%m-%d') for d in data]
     infected = np.array([d['totale_casi'] for d in data], dtype=np.int32)
-    growth_rate = (infected[1:] - infected[:-1]) / infected[:-1]
+    recovered = np.array([d['dimessi_guariti'] for d in data], dtype=np.int32)
+    dead = np.array([d['deceduti'] for d in data], dtype=np.int32)
+    hospitalized = np.array([d['ricoverati_con_sintomi'] for d in data], dtype=np.int32)
+    icus = np.array([d['terapia_intensiva'] for d in data], dtype=np.int32)
+    isolated = np.array([d['isolamento_domiciliare'] for d in data], dtype=np.int32)
+    currently_infected = np.array([d['totale_attualmente_positivi'] for d in data], dtype=np.int32)
+    tested = np.array([d['tamponi'] for d in data], dtype=np.int32)
 
+    new_infected = infected[-1] - infected[-2]
+    growth_rate = (infected[1:] - infected[:-1]) / infected[:-1]
     avg_growth_rate = moving_average(growth_rate, 5)
 
-    print(f"{dates[-1]} SUMMARY")
-    print(f"Total number of infected is {infected[-1]}")
-    print(f"Growth rate is {growth_rate[-1]:.2f}")
-    print(f"Average growth rate (last 5 days) is {avg_growth_rate[-1]:.2f}")
+    print("-" * 50)
+    print(f"{str(dates[-1])[:10]} Report")
+    print("-" * 50)
+    print(f"Total number of infected individuals is {infected[-1]}")
+    print(f"Total number of recovered individuals is {recovered[-1]}")
+    print(f"Total number of dead individuals is {dead[-1]}")
+    print(f"Total number of tested individuals is {tested[-1]}")
+    print("-" * 50)
+    print(f"Current number of infected individuals is {currently_infected[-1]}")
+    print(f"--- hospitalized individuals is {hospitalized[-1]}")
+    print(f"--- hospitalized individuals in ICU is {icus[-1]}")
+    print(f"--- home isolated individuals is {isolated[-1]}")
+    print("-" * 50)
+    print(f"Number of new infected is {new_infected}")
+    print(f"Growth rate is {growth_rate[-1]:.2f} (5 days smoothing is {avg_growth_rate[-1]:.2f})")
+    print("-" * 50)
 
-    fig, ((ax1, ax2), (ax3, ax4)) = plt.subplots(nrows=2, ncols=2, figsize=(15, 15))
-    ax1.plot(dates, infected)
-    fig.autofmt_xdate()
-    ax1.set_xlabel('time')
-    ax1.set_ylabel('infected')
-    ax1.set_title('infected over time')
+    plt.rc('lines', linewidth=3, markersize=8)
+    plt.rc('font', size=12)
 
-    ax2.plot(dates, infected)
-    ax2.set_yscale('log')
-    ax2.set_xlabel('time')
-    ax2.set_ylabel('infected (log scale)')
-    ax2.set_title('infected over time\n(log scale)')
+    with plt.xkcd():
+        fig, ((ax1, ax2, ax3), (ax4, ax5, ax6)) = plt.subplots(nrows=2, ncols=3, figsize=(15, 15))
 
-    ax3.plot(dates[1:], growth_rate)
-    ax3.set_xlabel('time')
-    ax3.set_ylabel('growth rate')
-    ax3.set_title('growth rate over time')
+        ax1.plot(dates, infected, 'o-', label='total infected')
+        ax1.plot(dates, recovered, 'o-', label='total recovered')
+        ax1.plot(dates, dead, 'o-', label='total dead')
+        ax1.legend(loc='upper left')
 
-    ax4.plot(dates[5:], avg_growth_rate)
-    ax4.set_xlabel('time')
-    ax4.set_ylabel('average growth rate')
-    ax4.set_title('average (last 5 days) growth rate over time')
-    plt.show()
+        fig.autofmt_xdate()
+
+        ax2.plot(dates, infected, 'o-', label='total infected\nin log scale')
+        ax2.set_yscale('log')
+        ax2.legend(loc='upper left')
+
+        ax3.plot(dates, currently_infected, 'o-', label='currently infected')
+        ax3.legend(loc='upper left')
+
+        ax4.plot(dates[1:], growth_rate, 'o-', label='growth rate')
+        ax4.plot(dates[5:], avg_growth_rate, label='5 days smoothing')
+        ax4.legend(loc='upper right')
+
+        ax5.plot(dates, hospitalized, 'o-', label='hospitalized')
+        ax5.plot(dates, icus, 'o-', label='hospitalized (in ICU)')
+        ax5.plot(dates, isolated, 'o-', label='home isolation')
+        ax5.legend(loc='upper left')
+
+        ax6.plot(dates, tested, 'o-', label='total tested')
+        ax6.legend(loc='upper left')
+
+        plt.show()
